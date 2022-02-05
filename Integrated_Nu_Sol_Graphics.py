9af5a7ee66f45818153cfeea4c508d1671c90b64
Max Mueggler <maximumsomething@gmail.com>
2022-02-05 14:17:25 -0500

    Slider on all 2D plots

--- Integrated_Nu_Sol_Graphics.py
+++ Integrated_Nu_Sol_Graphics.py
@@ -10,6 +10,7 @@ np.set_printoptions(threshold=sys.maxsize)
 
 import generate_potential as gp
 
+
 def ZGraphicalGenerate(ProjectName, XLEVEL, YLEVEL):
 	g = gp.GridInfo.load(ProjectName, 3)
 	g.NDIM = 1
@@ -21,17 +22,20 @@ def ZGraphicalGenerate(ProjectName, XLEVEL, YLEVEL):
 	zgrid = np.linspace(g.ZMIN, g.ZMAX, g.ZDIV)
 
 
-def Contour(ProjectName, ZLEVEL, MINLEV=-.15, MAXLEV=.08, DIV=25):
+# type is "contour", "heat", or "surface"
+def Graph2D(Type, ProjectName, ZLEVEL):
 	if type(ZLEVEL) != float:
 		print("ZLEVEL is not in float format.")
-	elif type(MINLEV) != float:
-		print("MINLEV is not in float format.")
-	elif type(MAXLEV) != float:
-		print("MAXLEV is not in float format.")
-	elif type(DIV) != int:
-		print("DIV is not in an integer format. Make sure there are no decimals.")
-	elif DIV <=0:
-		print("DIV cannot be less than or equal to zero.")
+	# elif type(MINLEV) != float:
+	# 	print("MINLEV is not in float format.")
+	# elif type(MAXLEV) != float:
+	# 	print("MAXLEV is not in float format.")
+	# elif type(DIV) != int:
+	# 	print("DIV is not in an integer format. Make sure there are no decimals.")
+	# elif DIV <=0:
+	# 	print("DIV cannot be less than or equal to zero.")
+	elif Type != 'contour' and Type != 'heat' and Type != 'surface':
+		print('type must be "contour", "heat", or "surface"')
 	else: 
 		g = gp.GridInfo.load(ProjectName, 3)
 		g.NDIM = 2
@@ -42,7 +46,11 @@ def Contour(ProjectName, ZLEVEL, MINLEV=-.15, MAXLEV=.08, DIV=25):
 		
 		meshx, meshy = np.meshgrid(xgrid, ygrid, sparse=False, indexing="xy")
 
-		fig, contour_axis = plt.subplots()
+		if Type == "surface":
+			fig = plt.figure()
+			axis = fig.add_subplot(111, projection='3d')
+		else:
+			fig, axis = plt.subplots()
 
 		zSlider = widgets.Slider(
 			ax=plt.axes([0.25, 0.0, 0.65, 0.03]), 
@@ -55,12 +63,18 @@ def Contour(ProjectName, ZLEVEL, MINLEV=-.15, MAXLEV=.08, DIV=25):
 		def update(val):
 			print("Generating for", val)
 			g.ZLEVEL = val
-			newGrid = gp.generate(ProjectName, g, Overwrite = True, PrintAnalysis = False)
+			V = gp.generate(ProjectName, g, Overwrite = True, PrintAnalysis = False)
 
-			contour_axis.clear()
+			axis.clear()
 
-			CS1 = contour_axis.contour(meshx, meshy, newGrid)
-			contour_axis.clabel(CS1, fontsize = 6)
+			if Type == 'contour':
+				plotHandle = axis.contour(meshx, meshy, V)
+				axis.clabel(plotHandle, fontsize = 6)
+			elif Type == 'heat':
+				axis.pcolormesh(meshx, meshy, V)
+				axis.set_aspect('equal')
+			elif Type == 'surface':
+				axis.plot_surface(meshx, meshy, V)
 
 			plt.draw()
 
@@ -72,32 +86,6 @@ def Contour(ProjectName, ZLEVEL, MINLEV=-.15, MAXLEV=.08, DIV=25):
 		plt.show()
 
 
-
-
-def Heat(ProjectName, ZLEVEL):
-	if type(ZLEVEL) != float:
-		print("ZLEVEL is not in float format.")
-	else:
-		
-		GraphicalGenerate(ProjectName, ZLEVEL)
-
-		fig,ax = plt.subplots()
-		ax.pcolormesh(meshx, meshy, V)
-		ax.set_aspect('equal')
-		plt.show()
-
-
-def Surface(ProjectName, ZLEVEL):
-	if type(ZLEVEL) != float:
-		print("ZLEVEL is not in float format.")
-	else:
-		
-		GraphicalGenerate(ProjectName, ZLEVEL)
-
-		figsur = plt.figure()
-		axsur = figsur.add_subplot(111, projection='3d')
-		axsur.plot_surface(meshx, meshy, V)
-		plt.show()
 		
 def PotentialZGraphics(ProjectName, XLEVEL, YLEVEL):
 	if type(YLEVEL) != float:
